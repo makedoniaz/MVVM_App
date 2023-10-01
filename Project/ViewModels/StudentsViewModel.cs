abede46c4255ecdf8af771b2e3ff58d29f9dd440
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 12:58:30 +0400

    Add ListView in StudentView

--- Project/ViewModels/StudentsViewModel.cs
+++ Project/ViewModels/StudentsViewModel.cs
@@ -1,7 +1,16 @@
-﻿using Project.ViewModels.Base;
+﻿using Project.Models;
+using Project.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace Project.ViewModels;
 
 public class StudentsViewModel : ViewModelBase
 {
+
+    public ObservableCollection<Student> Students { get; set; }
+
+    public StudentsViewModel()
+    {
+        this.Students = new ObservableCollection<Student>();
+    }
 }
