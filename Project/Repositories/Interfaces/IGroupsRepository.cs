c718577aa20601d91e646c010f52b282c079d243
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 12:37:04 +0400

    Add repository interfaces

---
+++ Project/Repositories/Interfaces/IGroupsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Project.Models;
+using System.Collections.Generic;
+
+namespace Project.Repositories.Interfaces;
+
+public interface IGroupsRepository
+{
+    IEnumerable<Group> GetAll();
+}
---
+++ Project/Repositories/Interfaces/ITeachersRepository.cs
@@ -0,0 +1,9 @@
+﻿using Project.Models;
+using System.Collections.Generic;
+
+namespace Project.Repositories.Interfaces;
+
+public interface ITeachersRepository
+{
+    IEnumerable<Teacher> GetAll();
+}
