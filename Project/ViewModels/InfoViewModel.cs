a52a3835b5c6066fa5a36ca5c90cdbae0e0cc10e
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 13:47:38 +0400

    Add info page layout and logic

--- Project/ViewModels/InfoViewModel.cs
+++ Project/ViewModels/InfoViewModel.cs
@@ -1,9 +1,15 @@
-﻿using Project.ViewModels.Base;
-
+﻿using Project.Command.Base;
+using Project.ViewModels.Base;
+using System;
 
 namespace Project.ViewModels;
 
 public class InfoViewModel : ViewModelBase
 {
+    private string NameInput { get; set; }
 
+    private CommandBase saveCommand;
+    public CommandBase SaveCommand => this.saveCommand ??= new CommandBase(
+            execute: () => Console.Write("TEST"),
+            canExecute: () => true);
 }
