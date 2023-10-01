bb82f09fc63797ebdc31ff7339a3e70b1db8e768
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 10:44:29 +0400

    Add models

---
+++ Project/Models/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace Project.Models;
+
+public class Group
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public int TeacherId { get; set; }
+}
---
+++ Project/Models/Teacher.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.Models;
+
+public class Teacher
+{
+    public int Id { get; set; }
+
+    public required string Fullname { get; set; }
+}
