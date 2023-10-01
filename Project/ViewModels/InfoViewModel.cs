dec747caa8e1d0535014f16467d03d95a02519a3
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 12:02:55 +0400

    Add main overlay on window

--- Project/ViewModels/InfoViewModel.cs
+++ Project/ViewModels/InfoViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Project.ViewModels.Base;
 
-namespace Project.ViewModels
+
+namespace Project.ViewModels;
+
+public class InfoViewModel : ViewModelBase
 {
-    internal class InfoViewModel
-    {
-    }
+
 }
