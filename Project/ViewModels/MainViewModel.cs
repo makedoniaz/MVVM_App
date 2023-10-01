d455c4eda405d3841d06f06a0e8833ad09726dbb
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 12:15:58 +0400

    Add commands and attach them to the main window buttons

--- Project/ViewModels/MainViewModel.cs
+++ Project/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Project.ViewModels.Base;
+﻿using Project.Command.Base;
+using Project.ViewModels.Base;
 
 namespace Project.ViewModels;
 
@@ -13,5 +14,30 @@ public class MainViewModel : ViewModelBase
         get => activeViewModel;
         set => base.PropertyChangeMethod(out activeViewModel, value);
     }
+    #endregion
+
+    #region Commands
+    private CommandBase infoCommand;
+    public CommandBase InfoCommand => this.infoCommand ??= new CommandBase(
+            execute: () => this.ActiveViewModel = new InfoViewModel(),
+            canExecute: () => true);
+
+
+    private CommandBase groupsCommand;
+    public CommandBase GroupsCommand => this.groupsCommand ??= new CommandBase(
+            execute: () => this.ActiveViewModel = new GroupsViewModel(),
+            canExecute: () => true);
+
+
+    private CommandBase studentsCommand;
+    public CommandBase StudentsCommand => this.studentsCommand ??= new CommandBase(
+            execute: () => this.ActiveViewModel = new StudentsViewModel(),
+            canExecute: () => true);
+
+
+    private CommandBase teachersCommand;
+    public CommandBase TeachersCommand => this.teachersCommand ??= new CommandBase(
+            execute: () => this.ActiveViewModel = new TeachersViewModel(),
+            canExecute: () => true);
     #endregion
 }
