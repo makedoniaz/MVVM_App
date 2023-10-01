12d48f13123a04a6487b59adf31ca4a8d7f83cd6
makedoniaz <jafar.sadig04@gmail.com>
2023-10-01 15:48:48 +0400

    Add loading and saving to json

---
+++ Project/Models/AcademyOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Project.Models;
+
+public class AcademyOptions : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private string? name;
+    private string? adress;
+
+    public string? Name {
+        get => name;
+        set => PropertyChangeMethod(out name, value);
+    }
+
+    public string? Adress {
+        get => adress;
+        set => PropertyChangeMethod(out adress, value);
+    }
+
+
+    protected void PropertyChangeMethod<T>(out T field, T value, [CallerMemberName] string propName = "")
+    {
+        field = value;
+
+        if (this.PropertyChanged != null)
+            this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
--- Project/ViewModels/InfoViewModel.cs
+++ Project/ViewModels/InfoViewModel.cs
@@ -1,15 +1,54 @@
 ﻿using Project.Command.Base;
+using Project.Models;
 using Project.ViewModels.Base;
 using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
 
 namespace Project.ViewModels;
 
 public class InfoViewModel : ViewModelBase
 {
-    private string NameInput { get; set; }
+
+    private AcademyOptions options = new AcademyOptions();
+    public AcademyOptions Options {
+        get => options;
+        set => base.PropertyChangeMethod(out options, value);
+    }
+
+    private readonly string infoPath = "Json\\academyOptions.json";
+
+    public InfoViewModel()
+    {
+        LoadInfo();
+    }
 
     private CommandBase saveCommand;
     public CommandBase SaveCommand => this.saveCommand ??= new CommandBase(
-            execute: () => Console.Write("TEST"),
-            canExecute: () => true);
+            execute: () => SaveInfo(),
+    canExecute: () => true);
+
+
+    private void LoadInfo()
+    {
+        if (File.ReadAllText(infoPath) == string.Empty || !File.Exists(infoPath))
+            return;
+
+        string json = File.ReadAllText(infoPath);
+        this.options = JsonSerializer.Deserialize<AcademyOptions>(json);
+    }
+
+    private void SaveInfo()
+    {
+        if (!File.Exists(infoPath))
+            File.Create(infoPath);
+
+        string json = JsonSerializer.Serialize(Options);
+        Console.WriteLine(json);
+
+        File.WriteAllText(infoPath, json);
+    }
 }
